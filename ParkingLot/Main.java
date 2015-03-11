00faab5f621e2627993a36ea38470b96e043661e
malencar <antoniomba.developer@gmail>
2015-03-10 21:58:51 -0400

    initial commit for Problem resolution

---
+++ ParkingLot/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int C,N,P,Q;
+		/* C = Length in meters of Parking Lot
+		 * N= Number of events (Arrivals or Departures)
+		 * P = Vehicle Plate
+		 * Q = Vehicle Length*/
+		  
+		
+	}
+
+}
