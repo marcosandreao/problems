3144c05c0b49c4b4a79362d1fb61fc54246bb815
malencar <antoniomba.developer@gmail>
2015-03-10 22:56:19 -0400

    Input C,N with scanner

--- ParkingLot/Main.java
+++ ParkingLot/Main.java
@@ -1,15 +1,25 @@
 
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*// TODO Auto-generated method stub
 		int C,N,P,Q;
-		/* C = Length in meters of Parking Lot
+		String C,S;
+		 C = Length in meters of Parking Lot
 		 * N= Number of events (Arrivals or Departures)
 		 * P = Vehicle Plate
-		 * Q = Vehicle Length*/
+		 * Q = Vehicle Length
 		  
-		
+		Scanner read = new Scanner(System.in);
+		System.out.printf('Enter [Length in Meters] and [Number of events] separated by a blank space, and HIT ENTER :');
+		//C = read.nextInt();
+		//N = read.nextInt();
+		System.out.println("C = "+ C + ","+ "N ="+ N);
+		//System.exit(0);
+*/	
+		System.out.println("Teste");
 	}
 
 }
--- ParkingLot/src/Main.java
+++ ParkingLot/src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int C,N,P,Q;
+		String I,O; //I=In, O=Out
+		 /*C = Length in meters of Parking Lot
+		 * N= Number of events (Arrivals or Departures)
+		 * P = Vehicle Plate
+		 * Q = Vehicle Length*/
+		  
+		Scanner read = new Scanner(System.in);
+		System.out.printf("Enter [Length in Meters] and [Number of events] separated by a blank space, and HIT ENTER :");
+		C = read.nextInt();
+		N = read.nextInt();
+		System.out.println(C + " "+ N);
+		//System.exit(0);
 	}
 
 }
